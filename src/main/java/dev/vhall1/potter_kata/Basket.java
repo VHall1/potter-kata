f78cda2aa0fea3d7290902ff66493a95dac015b5
VHall1 <victorhallpsn@hotmail.com>
2024-09-02 21:02:24 +0100

    tweak tests

--- src/main/java/dev/vhall1/potter_kata/Basket.java
+++ src/main/java/dev/vhall1/potter_kata/Basket.java
@@ -3,11 +3,13 @@ package dev.vhall1.potter_kata;
 public class Basket {
     private int[] books;
 
+    public static final int BOOK_VALUE_IN_PENCE = 800;
+
     public Basket(int[] books) {
         this.books = books;
     }
 
     public int calculateTotal() {
-        return this.books.length * 8;
+        return this.books.length * BOOK_VALUE_IN_PENCE;
     }
 }
--- src/test/java/dev/vhall1/potter_katta/BasketTests.java
+++ src/test/java/dev/vhall1/potter_katta/BasketTests.java
@@ -1,14 +1,36 @@
 package dev.vhall1.potter_katta;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import dev.vhall1.potter_kata.Basket;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BasketTests {
-    @Test
-    void noDiscount() {
-        Basket basket = new Basket(new int[0]);
-        assertEquals(0, basket.calculateTotal());
+    @Nested
+    @DisplayName("no discount is applied")
+    class NoDiscount {
+        @Test
+        @DisplayName("when no books are purchased")
+        void noBooks() {
+            Basket basket = new Basket(new int[]{});
+            assertEquals(0, basket.calculateTotal());
+        }
+
+        @Test
+        @DisplayName("when a single book is purchased")
+        void singleBook() {
+            Basket basket = new Basket(new int[]{1});
+            assertEquals(Basket.BOOK_VALUE_IN_PENCE, basket.calculateTotal());
+        }
+
+        @Test
+        @DisplayName("when the same book is purchased multiple times")
+        void threeRepeatedBooks() {
+            Basket basket = new Basket(new int[]{1, 1, 1});
+            assertEquals(Basket.BOOK_VALUE_IN_PENCE * 3, basket.calculateTotal());
+        }
     }
+
 }
