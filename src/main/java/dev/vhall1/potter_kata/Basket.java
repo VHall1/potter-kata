9f24e65b6133b0ae57470f1d53abab4b7f413aa8
VHall1 <victorhallpsn@hotmail.com>
2024-09-03 14:37:44 +0100

    fix discount logic + add grouping logic

--- src/main/java/dev/vhall1/potter_kata/Basket.java
+++ src/main/java/dev/vhall1/potter_kata/Basket.java
@@ -3,6 +3,7 @@ package dev.vhall1.potter_kata;
 
 import dev.vhall1.potter_kata.discount.DiscountFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Basket {
@@ -15,8 +16,28 @@ public class Basket {
     }
 
     public double calculateTotal() {
-        return this.books.size() * BOOK_VALUE_IN_PENCE * DiscountFactory.calculateDiscount(this.books);
+        var groupedBooks = this.groupBooks();
+        double total = 0;
+        for (var bookGroup : groupedBooks) {
+            var discount = DiscountFactory.calculateDiscount(bookGroup);
+            total += bookGroup.size() * BOOK_VALUE_IN_PENCE * discount;
+        }
+        return total;
     }
 
+    private List<List<Integer>> groupBooks() {
+        var groupedBooks = new ArrayList<List<Integer>>();
 
+        mainLoop:
+        for (var book : this.books) {
+            for (var bookGroup : groupedBooks) {
+                if (!bookGroup.contains(book)) {
+                    bookGroup.add(book);
+                    continue mainLoop;
+                }
+            }
+            groupedBooks.add(new ArrayList<>(List.of(book)));
+        }
+        return groupedBooks;
+    }
 }
